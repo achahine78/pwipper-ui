87ea7cbb82c4e83a267a5a12693acc41da7e61e6
achahine78 <achahine78@gmail.com>
2023-12-02 17:32:15 -0500

    feat: implement basic login functionality

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,12 +1,58 @@
+import { useState } from "react";
 import "./LoginForm.scss";
+import { FlexContainer } from "./ui/FlexContainer";
 import { FormInput } from "./ui/FormInput";
+import { addBearerToken, privateAxios } from "../api";
+import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
+  const navigate = useNavigate();
+  const [handle, setHandle] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    privateAxios
+      .post("/login", {
+        handle,
+        password,
+      })
+      .then(({ data }) => {
+        if (data?.token) {
+          localStorage.setItem("token", data?.token);
+          addBearerToken(data?.token);
+          navigate("/home");
+        }
+      })
+      .catch(({ response }) => {
+        const { data } = response;
+        if (data.message) {
+          console.error(data.message);
+        } else {
+          console.error("Something went wrong. Please try again later.");
+        }
+      });
+  };
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={onSubmit}>
       <div className="login-form__title">Login to your account</div>
-      <FormInput label="Handle" />
-      <FormInput label="Password" type="password" />
+      <FormInput
+        label="Handle"
+        onChange={(e) => {
+          setHandle(e.target.value);
+        }}
+      />
+      <FormInput
+        label="Password"
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <FlexContainer justifyContent="flex-end">
+        <button type="submit" className="login-form__button">
+          Login
+        </button>
+      </FlexContainer>
     </form>
   );
 };
--- src/components/ui/FormInput.tsx
+++ src/components/ui/FormInput.tsx
@@ -3,12 +3,13 @@ import "./FormInput.scss";
 type Props = {
   type?: "text" | "password";
   label: string;
+  onChange?: (value: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const FormInput = ({ type = "text", label }: Props) => {
+export const FormInput = ({ type = "text", label, onChange }: Props) => {
   return (
     <div className="form-input">
-      <input className="form-input__input" type={type} />
+      <input className="form-input__input" type={type} onChange={onChange} />
       <span className="form-input__label">{label}</span>
     </div>
   );
