caab37ef0c835c704247b4ddc23a65f2840cdd53
achahine78 <achahine78@gmail.com>
2024-02-19 19:54:56 -0500

    feat: add call to get timeline in homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import { PostPweep } from "../components/PostPweep";
 import { PweepList } from "../components/PweepList";
-import { pweeps, user } from "../mockData";
+import { user } from "../mockData";
+import { privateAxios } from "../api";
 
 export const Homepage = () => {
+  useEffect(() => {
+    privateAxios.get("/api/timeline").then(({ data }) => {
+      setPweeps(data.timeline);
+    });
+  }, []);
+  const [pweeps, setPweeps] = useState([]);
   return (
     <div>
       <PostPweep user={user} />
